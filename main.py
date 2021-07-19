3156ffa668f925188a5026624dbf3fc346f616b6
SNIKRO <snikr@bk.ru>
2021-07-20 02:31:01 +0600

    Build application

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask
+import os
+from flask import Flask, g
+from controllers.users import users_page
+from controllers.posts import posts_page
 
+config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+
+# создаём наше маленькое приложение :)
 app = Flask(__name__)
+app.config.from_json(config_path)
+app.register_blueprint(users_page)
+app.register_blueprint(posts_page)
 
 
-@app.route('/')
-def hello_world():
-    return "Hello World"
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 
 if __name__ == '__main__':
